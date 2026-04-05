cf40d8ffe3f30b42f9cd54ae5e44c7708fc7ab12
James Tippett <james.tippett@gmail.com>
2026-04-05 15:47:22 +0700

    feat: add Timer with exponential backoff

---
+++ spec/phoenix/timer_spec.cr
@@ -0,0 +1,63 @@
+require "../spec_helper"
+
+describe Phoenix::Timer do
+  describe "#schedule_timeout" do
+    it "calls the callback after the computed delay" do
+      called = false
+      timer = Phoenix::Timer.new(
+        callback: ->{ called = true },
+        timer_calc: ->(tries : Int32) { 1.millisecond },
+      )
+
+      timer.schedule_timeout
+      sleep 10.milliseconds
+      called.should be_true
+    end
+
+    it "increments tries on each schedule" do
+      recorded_tries = [] of Int32
+      timer = Phoenix::Timer.new(
+        callback: ->{ },
+        timer_calc: ->(tries : Int32) { recorded_tries << tries; 1.millisecond },
+      )
+
+      timer.schedule_timeout
+      sleep 5.milliseconds
+      timer.schedule_timeout
+      sleep 5.milliseconds
+      recorded_tries.should eq([1, 2])
+    end
+  end
+
+  describe "#reset" do
+    it "resets the try count" do
+      recorded_tries = [] of Int32
+      timer = Phoenix::Timer.new(
+        callback: ->{ },
+        timer_calc: ->(tries : Int32) { recorded_tries << tries; 1.millisecond },
+      )
+
+      timer.schedule_timeout
+      sleep 5.milliseconds
+      timer.schedule_timeout
+      sleep 5.milliseconds
+      timer.reset
+
+      timer.schedule_timeout
+      sleep 5.milliseconds
+      recorded_tries.should eq([1, 2, 1])
+    end
+  end
+
+  describe "DEFAULT_BACKOFF" do
+    it "returns increasing durations" do
+      calc = Phoenix::Timer::DEFAULT_BACKOFF
+      calc.call(1).should eq(1.second)
+      calc.call(2).should eq(2.seconds)
+      calc.call(3).should eq(5.seconds)
+      calc.call(4).should eq(10.seconds)
+      calc.call(5).should eq(10.seconds)
+      calc.call(100).should eq(10.seconds)
+    end
+  end
+end
--- src/phoenix/timer.cr
+++ src/phoenix/timer.cr
@@ -1,2 +1,34 @@
 module Phoenix
+  class Timer
+    BACKOFF_STEPS = [1, 2, 5, 10]
+
+    DEFAULT_BACKOFF = ->(tries : Int32) : Time::Span {
+      BACKOFF_STEPS.fetch(tries - 1, BACKOFF_STEPS.last).seconds
+    }
+
+    @tries : Int32 = 0
+    @generation : Int32 = 0
+
+    def initialize(
+      @callback : Proc(Nil),
+      @timer_calc : Proc(Int32, Time::Span)
+    )
+    end
+
+    def schedule_timeout : Nil
+      @generation += 1
+      @tries += 1
+      gen = @generation
+      delay = @timer_calc.call(@tries)
+      spawn do
+        sleep delay
+        @callback.call if gen == @generation
+      end
+    end
+
+    def reset : Nil
+      @generation += 1
+      @tries = 0
+    end
+  end
 end
