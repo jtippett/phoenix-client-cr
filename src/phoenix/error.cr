4cba6dbac5c454c9003b2346aa172a33d232660a
James Tippett <james.tippett@gmail.com>
2026-04-05 15:45:51 +0700

    feat: add error types

--- src/phoenix/error.cr
+++ src/phoenix/error.cr
@@ -1,2 +1,15 @@
 module Phoenix
+  class Error < Exception; end
+
+  class ClosedError < Error
+    def initialize
+      super("Cannot push to closed socket")
+    end
+  end
+
+  class AlreadyJoinedError < Error
+    def initialize(topic : String)
+      super("Channel #{topic} has already been joined — create a new channel instance")
+    end
+  end
 end
